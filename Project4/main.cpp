cd0e888b3b3c940021914d3585b234ef1965c587
anaghasrivatsav <anagha.v.srivatsav@gmail.com>
2022-03-03 15:02:51 -0800

    Initial Commit

---
+++ Project4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project4
+//
+//  Created by Anagha Srivatsav on 3/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
